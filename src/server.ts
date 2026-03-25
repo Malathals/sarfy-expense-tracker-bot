a447cf33b5e5377bfa2ff6d6d3fae36f6f9aafeb
Malath A. <malathas14@gmail.com>
2026-03-26 01:34:15 +0300

    fix: prisma

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import { prisma } from './lib/prisma';
 
 const app = express();
-const PORT = 3000;
+const PORT = 2020;
 
 app.get('/', (_req, res) => {
+  console.log('Received request to root endpoint');
   res.send('Telegram Expense Bot API is running');
+});
+
+app.get('/expenses', async (_req, res) => {
+  console.log('Fetching expenses from the database...');
+  const expenses = await prisma.expense.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  res.json(expenses);
 });
 
 app.listen(PORT, () => {
